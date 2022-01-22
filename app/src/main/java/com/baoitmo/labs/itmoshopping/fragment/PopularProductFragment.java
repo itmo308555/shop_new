cb08446fd82e21f3f4b649d4b1c9aadb059aec58
baovn-2976 <74024040+baovn-2976@users.noreply.github.com>
2022-01-22 16:13:16 +0300

    new

---
+++ app/src/main/java/com/baoitmo/labs/itmoshopping/fragment/PopularProductFragment.java
@@ -0,0 +1,52 @@
+package com.baoitmo.labs.itmoshopping.fragment;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.baoitmo.labs.itmoshopping.R;
+import com.baoitmo.labs.itmoshopping.adapter.PopularProductAdapter;
+import com.baoitmo.labs.itmoshopping.helper.Data;
+
+public class PopularProductFragment extends Fragment {
+    RecyclerView pRecyclerView;
+    Data data;
+    private PopularProductAdapter pAdapter;
+
+    public PopularProductFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_popular, container, false);
+        data = new Data();
+        pRecyclerView = view.findViewById(R.id.popular_product_rv);
+        pAdapter = new PopularProductAdapter(data.getPopularList(), getContext(), "pop");
+        RecyclerView.LayoutManager pLayoutManager = new LinearLayoutManager(getContext());
+        pRecyclerView.setLayoutManager(pLayoutManager);
+        pRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        pRecyclerView.setAdapter(pAdapter);
+
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("Popular");
+    }
+}
---
+++ app/src/main/java/com/baoitmo/labs/itmoshopping/interfaces/AddorRemoveCallbacks.java
@@ -0,0 +1,12 @@
+package com.baoitmo.labs.itmoshopping.interfaces;
+
+
+
+public interface AddorRemoveCallbacks {
+
+    void onAddProduct();
+
+    void onRemoveProduct();
+
+    void updateTotalPrice();
+}
